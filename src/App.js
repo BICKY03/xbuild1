62ae24c46d25ad6b47d31d69f41e9d26c2eba254
BICKY03 <jotirajmajhi29@gmail.com>
2024-05-11 18:28:27 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchCountry();
+  }, []);
+
+  async function fetchCountry() {
+    try {
+      const response = await fetch("https://restcountries.com/v3.1/all");
+
+      if (!response.ok) {
+        throw new Error('Failed to fetch data');
+      }
+
+      const data = await response.json();
+      setCountries(data);
+      setLoading(false);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      setLoading(false);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Countries</h1>
+      {
+        loading ? (<p>Loading...</p>) : (
+          <ul className="country-list">
+            {
+              countries.map((country, index) => {
+                return (
+                  <li key={index} className="country-item">
+                  
+                    <img src={country.flags.png} alt={`Flag of ${country.name.common}`} className="country-flag" />
+                    <p className="country-name">{country.name.common}</p>
+                  </li>
+                );
+              })
+            }
+          </ul>
+        )
+      }
     </div>
   );
 }
